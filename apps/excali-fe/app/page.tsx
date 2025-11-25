6d3233432de28dde7cf7bf03c54b430160b0fb84
Abhishek Jain <172581933+Abhishek-168@users.noreply.github.com>
2025-11-25 23:31:30 +0530

    added initial core-features card

--- apps/excali-fe/app/page.tsx
+++ apps/excali-fe/app/page.tsx
@@ -4,21 +4,14 @@ import VideoPlayer from "./videoPlayer";
 export default function Home() {
   return (
     <>
-      <img src="./finalbg.jpg" alt="backgorund-img" className="absolute -z-10 " />
+      <img
+        src="./finalbg.jpg"
+        alt="backgorund-img"
+        className="absolute -z-10 "
+      />
       <NavBar />
-        <div className="flex flex-col justify-between relative left-[10vw] top-[5vw] w-[35vw] h-[22vw]">
-          <span className="text-white text-[4.5em] leading-[1.15] tracking-tight overflow-clip font-boldonse"> Real Time Collaborative Drawing Canvas </span>
-          <div className="flex">
-            <button className="bg-cyan-600 text-white p-3 pl-4 pr-4 border-0 rounded-xl m-2 cursor-pointer"> Get Started </button>
-            <button className="bg-black text-white p-3 pl-4 pr-4 border-0 rounded-xl m-2 cursor-pointer"> Guest Canvas </button>
-          </div>
-        </div>
-        <div className="absolute right-[19vw] top-[11vw] w-[16vw] h-[16vw]">
-          <VideoPlayer />
-        </div>
-         <div className="absolute right-[10vw] top-[20vw] w-[16vw] h-[16vw]">
-          <VideoPlayer />
-        </div>
+      <HeroSection />
+      <CoreFeatures />
     </>
   );
 }
@@ -27,7 +20,7 @@ function NavBar() {
   return (
     <nav className="flex justify-between items-center h-[6vw] w-full p-4 pl-[10vw] pr-[10vw]">
       <div>
-        <span className="text-3xl text-white">Excalidraw</span>
+        <span className="text-3xl text-white font-mono">SLATE</span>
       </div>
       <div>
         <Link
@@ -47,4 +40,46 @@ function NavBar() {
       </div>
     </nav>
   );
+}
+
+function HeroSection() {
+  return (
+    <>
+      <div className="flex flex-col justify-between relative left-[10vw] top-[5vw] w-[35vw] h-[22vw]">
+        <span className="text-white text-[4.5em] leading-[1.15] tracking-tight overflow-clip font-boldonse text-shadow-amber-50">
+          {" "}
+          Real Time Collaborative Drawing Canvas{" "}
+        </span>
+        <div className="flex">
+          <button className="bg-cyan-600 text-white p-3 pl-4 pr-4 border-0 rounded-xl m-2 cursor-pointer">
+            {" "}
+            Get Started{" "}
+          </button>
+          <button className="bg-black text-white p-3 pl-4 pr-4 border-0 rounded-xl m-2 cursor-pointer">
+            {" "}
+            Guest Canvas{" "}
+          </button>
+        </div>
+      </div>
+      <div className="absolute right-[19vw] top-[11vw] w-[16vw] h-[16vw]">
+        <VideoPlayer />
+      </div>
+      <div className="absolute right-[10vw] top-[20vw] w-[16vw] h-[16vw]">
+        <VideoPlayer />
+      </div>
+    </>
+  );
+}
+
+function CoreFeatures() {
+  return(
+    <>
+      <div className="w-[70vw] h-[40vw] bg-black text-white rounded-4xl ml-[15vw] mr-[15vw] mt-[20vw]">
+        <span className="relative flex justify-self-center p-4 text-4xl font-bold feat-class"> Our Core Features </span>
+        <div>Feature.1</div>
+        <div>Feature.2</div>
+        <div>Feature.3</div>
+      </div>
+    </>
+  )
 }
--- apps/excali-fe/app/videoPlayer.tsx
+++ apps/excali-fe/app/videoPlayer.tsx
@@ -3,7 +3,7 @@
 export default function VideoPlayer() {
   return (
     <video src="./demoVid.mp4" autoPlay muted loop playsInline
-        className="border-2 rounded-xl"
+        className="border-0 rounded-4xl"
     ></video>
   );
 }
