d0cd3710db0951000834e6263ffef00eea3aaaa9
Abhishek Jain <172581933+Abhishek-168@users.noreply.github.com>
2025-11-26 20:42:17 +0530

    added new comp in home page

--- apps/excali-fe/app/page.tsx
+++ apps/excali-fe/app/page.tsx
@@ -5,13 +5,14 @@ export default function Home() {
   return (
     <>
       <img
-        src="./finalbg.jpg"
+        src="./main.jpg"
         alt="backgorund-img"
         className="absolute -z-10 "
       />
       <NavBar />
       <HeroSection />
       <CoreFeatures />
+      <img src="./footer-img.jpg" alt="" className="w-screen absolute -z-10 -mt-[15vw]" />
     </>
   );
 }
@@ -61,10 +62,10 @@ function HeroSection() {
           </button>
         </div>
       </div>
-      <div className="absolute right-[19vw] top-[11vw] w-[16vw] h-[16vw]">
+      <div className="absolute right-[19vw] top-[10vw] w-[18vw] h-[18vw]">
         <VideoPlayer />
       </div>
-      <div className="absolute right-[10vw] top-[20vw] w-[16vw] h-[16vw]">
+      <div className="absolute right-[10vw] top-[19vw] w-[18vw] h-[18vw]">
         <VideoPlayer />
       </div>
     </>
@@ -72,14 +73,56 @@ function HeroSection() {
 }
 
 function CoreFeatures() {
-  return(
+  return (
     <>
-      <div className="w-[70vw] h-[40vw] bg-black text-white rounded-4xl ml-[15vw] mr-[15vw] mt-[20vw]">
-        <span className="relative flex justify-self-center p-4 text-4xl font-bold feat-class"> Our Core Features </span>
-        <div>Feature.1</div>
-        <div>Feature.2</div>
-        <div>Feature.3</div>
+      <div className="w-[70vw] h-full bg-black text-white rounded-4xl ml-[15vw] mr-[15vw] mt-[20vw]">
+        <span className="relative flex justify-self-center p-4 text-4xl font-bold feat-class">
+          {" "}
+          Our Core Features{" "}
+        </span>
+        <div>
+          <Feature title="Modern Drawing Canvas"/>
+        </div>
+        <div>
+          <Feature title="Synched Collaborative Canvas"/>
+        </div>
+        <div>
+          <Feature title="Create Rooms Public or Private"/>     
+        </div>
       </div>
     </>
-  )
+  );
+}
+
+function Feature( {title } : {
+  title : string
+}) {
+  return (
+    <>
+      <div className="flex items-center p-8 w-[70vw] mx-auto rounded-xl gap-10 pl-[4vw] mt-[1.6vw]">
+
+        <div className="flex items-center gap-10">
+          <div className="w-[3px] h-[9vw] bg-orange-400" />
+
+          <div className="w-[13vw]">
+            <VideoPlayer />
+          </div>
+        </div>
+
+
+        <div className="flex flex-col max-w-[37vw] ml-[4vw]">
+          <h2 className="text-3xl font-boldonse text-white">
+            {title}
+          </h2>
+          <p className="text-white mt-2 text-sm leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            temporibus, doloremque accusantium amet animi provident. Quos quasi
+            nulla, doloremque quae! A eaque quos tempora adipisci, excepturi rem
+            laudantium assumenda dolores sed quis esse praesentium at quia
+            inventore dolor consequuntur!
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
